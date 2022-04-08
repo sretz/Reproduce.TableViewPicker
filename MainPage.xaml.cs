4c12b11b01148810553cf1f023f6b04ea54daa60
Steven Retz <sretz@innovoft.com>
2022-04-08 17:00:01 -0300

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Reproduce.TableViewPicker;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+
+		canon.ItemsSource = new string[]
+		{
+			"Independence",
+			"Jerusalem",
+		};
+		//canon.SelectedItem = "Independence";
+	}
+
+	protected override void OnDisappearing()
+	{
+		base.OnDisappearing();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Reproduce.TableViewPicker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
